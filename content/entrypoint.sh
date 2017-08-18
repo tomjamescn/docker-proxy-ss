c9c47d4ff50ca449a37a8b8f9b045c0b74b38124
tomjamescn <tomjamescn@gmail.com>
2017-08-18 17:13:03 +0800

    bugfix

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -12,13 +12,14 @@ echo "$SERVER_ADDR $SERVER_PORT $PASSWORD $METHOD"
 
 (
 cat <<EOF
-{"method":$METHOD,"server":$SERVER_ADDR,"password":$PASSWORD,"server_port":$SERVER_PORT,"timeout":60,"local_address": "127.0.0.1","local_port": $LOCAL_SS_PORT}
+{"method":"$METHOD","server":"$SERVER_ADDR","password":"$PASSWORD","server_port":$SERVER_PORT,"timeout":60,"local_address": "127.0.0.1","local_port": $LOCAL_SS_PORT}
 EOF
 ) > /root/ss-local-config.json
 
+cat /root/ss-local-config.json
 
 #config privoxy
-sed -i 's/^listen-address.*$/listen-address  0.0.0.0:$PROXY_PORT/' /etc/privoxy/config
+sed -i 's/^listen-address.*$/listen-address  0.0.0.0:'$PROXY_PORT'/' /etc/privoxy/config
 
 (
 cat <<EOF
@@ -26,7 +27,9 @@ forward-socks5 / 127.0.0.1:$LOCAL_SS_PORT .
 EOF
 ) >> /etc/privoxy/config
 
-sslocal -c /root/ss-local-config.json -f /root/ss-local.pid
+privoxy --user privoxy /etc/privoxy/config
+
+sslocal -c /root/ss-local-config.json -d start
 
 sh
 
